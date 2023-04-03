5b581c9314ff11a11ab5f7d77f7e09630f09b2c4
grgr_smrkv <grgrsmrkv@gmail.com>
2023-04-03 12:06:17 +0300

    .gitignore add

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,6 +2,6 @@ public class BonusMilesService {
     public int calculate(int cost) {
         int bonusScale = 20;
         int bonusMile = cost / bonusScale;
-        return bonusMile; // возвращаем рассчитанное кол-во бонусных миль
+        return bonusMile;
     }
 }
